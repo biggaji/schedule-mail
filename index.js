a64a65d5f2bf400061fa5a71870976fc1551a0b8
biggaji <tobiajibade2017@yandex.com>
2023-06-30 15:33:38 +0100

    cron job sents mail every 2 minute

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-const cronJob = require("cron").CronJob;
-
-function jobStatusStart() {
-    console.log("Job started")
-};
-
-function jobStatusEnd() {
-    console.log("JOB ENDED");
+const { config } = require("dotenv");
+if (process.env.NODE_ENV !== "production") {
+	config();
 }
 
-// to run every month */1
-const schedOptions = {
-    cronTime: '*/2 14 24 */1 4',
-    onTick: jobStatusStart,
-    onComplete: jobStatusEnd,
-    timeZone: 'Africa/Lagos'
-}
+const cron = require("node-cron");
+const nodemailer = require("nodemailer");
 
-const job = new cronJob(schedOptions);
-job.start();
+// send an email every 2 minute
+try {
+	cron.schedule("*/2 * * * *", async function() {			
+		try {
+			const mailTransport = nodemailer.createTransport({
+				host: process.env.SMTP_HOST,
+				port: parseInt(process.env.SMTP_PORT),
+				auth: {
+					user: process.env.SMTP_USER,
+					pass: process.env.SMTP_PASS
+				}
+			});
+	
+			const mailReceiver = process.env.MAIL_RECIEVER;
+			let delivered = false;
+	
+			const mailObject = {
+				from: "'Node Cron Test' <nodecron.test@mail.com>",
+				to: `${mailReceiver}`,
+				subject: "Testing node cron",
+				html: `
+					<h1>Do cron job work in production?</h1>
+					<p>If this mail delivers every 2 mins, then it works, else it doesn't.</p>
+				`
+			};
+	
+			const mailResponse = await mailTransport.sendMail(mailObject);
+	
+			if (mailResponse.accepted.length === 1 && mailResponse.accepted[0] === mailReceiver) {
+				delivered = true;
+				console.log("mail delivered")
+			} else {
+				console.log("mail delivery failed");
+			};
+	
+			console.log(delivered);
+		} catch (error) {
+			console.error("An error occured while sending mail", error);
+		}
+	},
+	{ timezone: "Africa/Lagos" });
+} catch (error) {
+	console.error('An error occured while setting up cron job', error);
+}
